626e5bcd1488cc0f7235e212d9ab96ad0fb24e2c
jhonxarox <jhonarox22@gmail.com>
2025-07-02 20:50:35 +0800

    feat: add shared preference

--- lib/presentation/viewmodels/saved_books_provider.dart
+++ lib/presentation/viewmodels/saved_books_provider.dart
@@ -1,34 +1,44 @@
-import 'package:circe/data/models/book_model.dart';
+import 'package:circe/core/constants.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SavedBooksProvider extends StateNotifier<Set<BookModel>> {
-  SavedBooksProvider() : super({});
+class SavedBooksProvider extends StateNotifier<Set<int>> {
+  SavedBooksProvider() : super({}) {
+    _loadSavedBookIds();
+  }
 
-  bool isSaved(BookModel book) => state.any((b) => b.id == book.id);
+  Future<void> _loadSavedBookIds() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final List<String>? idStrings =
+        prefs.getStringList(SharedPreferencesKeys.savedBookIds);
 
-  void toggle(BookModel book) {
-    final BookModel existing = state.firstWhere(
-      (b) => b.id == book.id,
-      orElse: () => BookModel(
-        id: -1,
-        title: '',
-        authors: [],
-        summaries: [],
-        formats: {},
-        downloadCount: 0,
-      ),
-    );
-
-    if (existing.id != -1) {
-      state = {...state}..remove(existing);
-    } else {
-      state = {...state}..add(book);
+    if (idStrings != null) {
+      final ids = idStrings.map(int.parse).toSet();
+      state = ids;
     }
+  }
+
+  bool isSaved(int bookId) => state.contains(bookId);
+
+  Future<void> toggle(int bookId) async {
+    final prefs = await SharedPreferences.getInstance();
+    final newState = {...state};
+
+    if (newState.contains(bookId)) {
+      newState.remove(bookId);
+    } else {
+      newState.add(bookId);
+    }
+
+    state = newState;
+    await prefs.setStringList(
+      SharedPreferencesKeys.savedBookIds,
+      newState.map((id) => id.toString()).toList(),
+    );
   }
 }
 
-final StateNotifierProvider<SavedBooksProvider, Set<BookModel>>
-    savedBooksProvider =
-    StateNotifierProvider<SavedBooksProvider, Set<BookModel>>(
+final StateNotifierProvider<SavedBooksProvider, Set<int>> savedBooksProvider =
+    StateNotifierProvider<SavedBooksProvider, Set<int>>(
   (ref) => SavedBooksProvider(),
 );
